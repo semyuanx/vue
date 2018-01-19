409819f2c17868ec9d3d01484d1e80185b42d063
sem <steventbet@gmail.com>
2018-01-19 17:09:32 +0800

    init todo-list

---
+++ vue-todolist/src/header.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="header">
+        <h1>todoList</h1>
+        <input @keydown.enter.stop="addData()" placeholder="接下来要做什么?" autofocus="autofocus" class="new-todo">
+    </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  methods: {
+      addData: function() {
+        alert(123);
+      }
+  }
+};
+</script>
---
+++ vue-todolist/src/todolist.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" >
+    <div class="todoapp">
+      <Header></Header>    
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script scope>
+import "./assets/index.css";
+import Main from './body.vue';
+import Header from './header.vue';
+import Footer from './footer.vue';
+export default {
+  name: "Todolist",
+  components:{
+    Main,
+    Header,
+    Footer
+  }
+};
+</script>
